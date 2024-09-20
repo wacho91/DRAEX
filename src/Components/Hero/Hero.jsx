72d2c39bf2c8afe1eeb26c355e79b68ec93eb5a7
wacho91 <criskol.71@gmail.com>
2024-09-20 11:37:41 -0500

    Arquitecture component Hero base

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,6 +9,10 @@ const Hero = () => {
                     <h1 className="text-4xl font-bold font-serif">
                         DRAEX-S.A.S
                     </h1>
+                    <p className="text-sm md:text-base text-gray-500 leading-7">
+                        DRAEX-S.A.S es una empresa de consultoría y servicios de tecnología que
+                        ofrece soluciones innovadoras para mejorar la experiencia de los usuarios.
+                    </p>
                 </div>
             </div>
         </div> 
