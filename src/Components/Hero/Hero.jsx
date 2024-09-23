5dbb56a02eeb6531cc7253c02729b10f191f2ac9
wacho91 <criskol.71@gmail.com>
2024-09-23 11:12:46 -0500

    Changes in the hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <div className="container">
             <div className="grid grid-cols-1 md:grid-cols-2 md:min-h-[600px] gap-10">
                 {/*Information section*/}
-                <div className="flex flex-col justify-center gap-7 md:pl-12 xl:pl-52 text-center md:text-left pt-20 md:pt-0 px-10">
+                <div className="flex flex-col justify-center gap-7 md:pr-[50px] xl:pr-52 text-center md:text-left pt-10 md:pt-0 px-10">
                     <h1 className="text-4xl font-bold font-serif">
                         DRAEX-S.A.S
                     </h1>
