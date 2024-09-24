ca473cc1c4499ebdbe83ff28c6b8502163ff0faf
wacho91 <criskol.71@gmail.com>
2024-09-24 11:06:27 -0500

    Arquitecture Projects complete

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,8 +1,13 @@
 
 const Projects = () => {
   return (
-    <div>
-        Projects Components
+    <div className="container py-12">
+        {/*Heading Title*/}
+        <div className="space-y-2 text-center mx-w-[350px] mx-auto mb-8">
+            <h1 className="text-4xl font-bold">
+                Nuestros Servicios
+            </h1>
+        </div>
     </div>
   )
 }
