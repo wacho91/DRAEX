bcb733181415663d556696f7df12f7fa6d11beea
wacho91 <criskol.71@gmail.com>
2024-09-24 11:23:54 -0500

    Arquitecture Projects complete

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -8,6 +8,65 @@ const Projects = () => {
                 Nuestros Servicios
             </h1>
         </div>
+        {/*Card Section*/}
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
+                <div 
+                    className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
+                    hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
+                >
+
+                </div>
+
+                <div 
+                    className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
+                    hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
+                >
+
+                </div>
+
+                <div 
+                    className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
+                    hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
+                >
+
+                </div>
+
+                <div 
+                    className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
+                    hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
+                >
+
+                </div>
+
+                <div 
+                    className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
+                    hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
+                >
+
+                </div>
+
+                <div 
+                    className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
+                    hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
+                >
+
+                </div>
+
+                <div 
+                    className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
+                    hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
+                >
+
+                </div>
+
+
+                <div 
+                    className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
+                    hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
+                >
+
+                </div>
+            </div>
     </div>
   )
 }
