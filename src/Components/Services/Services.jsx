511da4942591b4bc1a277552fd26309a1aaa0d3c
wacho91 <criskol.71@gmail.com>
2024-10-02 11:44:56 -0500

    Changes

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,3 +1,4 @@
+import { Service } from "../../Data/Data"
 
 
 const Services = () => {
@@ -13,7 +14,23 @@ const Services = () => {
             </div>
             {/*Card Section*/}
             <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-                <div 
+                {
+                    Service.map((data) => {
+                        return (
+                            <div 
+                                key={data.id}
+                                className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
+                                hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
+                            >
+                                <h2 className=" text-center text-2xl font-bold font-serif">{data.title}</h2>
+                                <p>{data.item}</p>
+                            </div>
+                        )
+                    })
+                }
+                
+
+                {/* <div 
                     className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
                     hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
                 >
@@ -25,14 +42,7 @@ const Services = () => {
                     hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
                 >
 
-                </div>
-
-                <div 
-                    className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
-                    hover:text-gray-800 hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
-                >
-
-                </div>
+                </div> */}
 
                 {/* <div 
                     className="space-y-4 border-[1px] border-black/30 px-6 py-12 hover:bg-slate-300 
---
+++ src/Data/Data.jsx
@@ -0,0 +1,11 @@
+export const Service = [
+    {
+        id: 1,
+        title: "EXTRACCIÓN",
+        item: {
+            "1": "Arena y grava en ríos.",
+            "2": "Lirio",
+            "3": "oberturas acuáticas. "
+        }
+    }
+]
