884d62d65b1454dc63626381b338adaf31fc70c5
wacho91 <criskol.71@gmail.com>
2024-10-03 11:42:44 -0500

    Last Changes services component

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,82 +1,82 @@
 import { useState } from "react"
-// import { Service } from "../../Data/Data"
+import { Service } from "../../Data/Data"
 
 
-const Service = [
-    {
-        id: 1,
-        title: "EXTRACCIÓN",
-        item: {
-            "1": "Arena y grava en ríos.",
-            "2": "Lirio",
-            "3": "oberturas acuáticas. "
-        }
-    },
+// const Service = [
+//     {
+//         id: 1,
+//         title: "EXTRACCIÓN",
+//         item: {
+//             "1": "Arena y grava en ríos.",
+//             "2": "Lirio",
+//             "3": "oberturas acuáticas. "
+//         }
+//     },
 
-    {
-        id: 2,
-        title: "EXCAVACION & PILOTAJE",
-        item: {
-            "1": "Movimiento de tierra",
-            "2": "Pilotaje",
-            "3": "Excavación de pozos profundos en rellenos sanitarios. (para la extracción de gas metano)." 
-        }
-    },
+//     {
+//         id: 2,
+//         title: "EXCAVACION & PILOTAJE",
+//         item: {
+//             "1": "Movimiento de tierra",
+//             "2": "Pilotaje",
+//             "3": "Excavación de pozos profundos en rellenos sanitarios. (para la extracción de gas metano)." 
+//         }
+//     },
 
-    {
-        id: 3,
-        title: "EXCAVACION ESPECIALIZADA",
-        item: {
-            "1": "Limpieza y desazolve en algunas de sedimentación",
-            "2": "Canales y Rios"
-        }
-    },
+//     {
+//         id: 3,
+//         title: "EXCAVACION ESPECIALIZADA",
+//         item: {
+//             "1": "Limpieza y desazolve en algunas de sedimentación",
+//             "2": "Canales y Rios"
+//         }
+//     },
 
-    {
-        id: 4,
-        title: "DRENAGES",
-        item: {
-            "1": "Ríos",
-            "2": "Lagunas",
-            "3": "Canales",
-            "4": "Contrucción de Jarillones"
-        }
-    },
+//     {
+//         id: 4,
+//         title: "DRENAGES",
+//         item: {
+//             "1": "Ríos",
+//             "2": "Lagunas",
+//             "3": "Canales",
+//             "4": "Contrucción de Jarillones"
+//         }
+//     },
 
-    {
-        id: 5,
-        title: "MÁQUINAS PARA REALIZAR",
-        item: {
-            "1": "Pilotajes",
-            "2": "Dragados",
-            "3": "Excavaciones",
-            "4": "Perfilados de Hincamiento",
-            "5": "Suministro de tubería",
-            "6": "Transporta fluvial",
-            "7": "Remolcador fluvial",
-            "8": "Instalación de canasta de acero",
-            "9": "Instalación de tubería por Hincamiento con martillo de caída libre"
-        }
-    },
+//     {
+//         id: 5,
+//         title: "MÁQUINAS PARA REALIZAR",
+//         item: {
+//             "1": "Pilotajes",
+//             "2": "Dragados",
+//             "3": "Excavaciones",
+//             "4": "Perfilados de Hincamiento",
+//             "5": "Suministro de tubería",
+//             "6": "Transporta fluvial",
+//             "7": "Remolcador fluvial",
+//             "8": "Instalación de canasta de acero",
+//             "9": "Instalación de tubería por Hincamiento con martillo de caída libre"
+//         }
+//     },
 
-    {
-        id: 6,
-        title: "CONSTRUCCIÓN DE MUELLES FLUVIALES Y MARÍTIMOS",
-        item: {}
-    },
+//     {
+//         id: 6,
+//         title: "CONSTRUCCIÓN DE MUELLES FLUVIALES Y MARÍTIMOS",
+//         item: {}
+//     },
 
-    {
-        id: 7,
-        title: "PILOTES HINCADOS Y PRE EXCAVADOS PARA PUENTES",
-        item: {}
-    },
+//     {
+//         id: 7,
+//         title: "PILOTES HINCADOS Y PRE EXCAVADOS PARA PUENTES",
+//         item: {}
+//     },
 
-    {
-        id: 8,
-        title: "CIMENTACIONES PROFUNDAS PARA EDIFICIOS",
-        item: {}
-    }
-]
+//     {
+//         id: 8,
+//         title: "CIMENTACIONES PROFUNDAS PARA EDIFICIOS",
+//         item: {}
+//     }
+// ]
 
 
 const Services = () => {
@@ -99,7 +99,7 @@ const Services = () => {
                         <div
                             key={data.id}
                             className={`
-                                relative overflow-hidden rounded-lg shadow-lg transition-all duration-300 ease-in-out
+                                relative overflow-hidden rounded-lg shadow-lg transition-all duration-500 ease-in-out
                                 ${hoveredCard === data.id ? 'bg-white' : 'bg-gray-200'}
                                 ${hoveredCard === data.id ? 'h-auto' : 'h-24'}
                             `}
